787382e95567314ec3e6f3ab3c0af7b4e71392bd
JeonSangYong <tkddyd420@naver.com>
2023-01-08 18:41:28 +0900

    유스케이스 둘러보기

---
+++ BuckPal/src/main/java/io/reflectoring/buckpal/account/application/service/SendMoneyService.java
@@ -0,0 +1,21 @@
+package io.reflectoring.buckpal.account.application.service;
+
+import io.reflectoring.buckpal.account.application.port.in.SendMoneyUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+public class SendMoneyService implements SendMoneyUseCase {
+
+    private final LoadAccountPort loadAccountPort;
+    private final AccountLock accountLock;
+    private final UpdateAccountStatePort updateAccountStatePort;
+
+    @Override
+    public boolean sendMoney(SendMoneyCommand command) {
+        // TODO: 비즈니스 규칙 검증
+        // TODO: 모델 상태 조작
+        // TODO: 출력 값 반환
+    }
+}
