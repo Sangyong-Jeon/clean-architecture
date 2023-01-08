67ff7314c9a018212477fd321cfa84fb42cccb92
JeonSangYong <tkddyd420@naver.com>
2023-01-08 19:08:18 +0900

    입력 유효성 검증

---
+++ BuckPal/src/main/java/io/reflectoring/buckpal/account/application/port/in/SendMoneyCommand.java
@@ -0,0 +1,29 @@
+package io.reflectoring.buckpal.account.application.port.in;
+
+import io.reflectoring.buckpal.account.domain.Account.AccountId;
+import io.reflectoring.buckpal.account.domain.Money;
+import io.reflectoring.buckpal.common.SelfValidating;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
+
+    @NotNull
+    private final AccountId sourceAccountId;
+    @NotNull
+    private final AccountId targetAccountId;
+    @NotNull
+    private final Money money;
+
+    public SendMoneyCommand(
+            AccountId sourceAccountId,
+            AccountId targetAccountId,
+            Money money) {
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.money = money;
+        requireGraterThan(money, 0);
+        this.validateSelf();
+    }
+}
